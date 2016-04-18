378033d7ef56c9b8c2c720ef831e5daf8838337c
Adam LaPlante <adam.laplante@emc.com>
2016-04-18 08:16:26 -0600

    react ui work in progress

--- src/main/java/com/racejournal/controller/HomeController.java
+++ src/main/java/com/racejournal/controller/HomeController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/home")
+    @RequestMapping(value = "/")
     public String index() {
-        return "home";
+        return "app";
     }
 }
